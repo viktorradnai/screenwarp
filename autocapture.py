e4fae4d69c1b993f41e989876eb1dd4e1de58e82
Viktor Radnai <viktor.radnai@ticketmaster.co.uk>
2016-11-26 11:08:06 +0000

    Autocapture works up to running chessboard detection

--- autocapture.py
+++ autocapture.py
@@ -16,6 +16,7 @@ import wand.image
 import wand.color
 import wand.drawing
 import requests
+import traceback
 import numpy as np
 from threading import *
 
@@ -23,6 +24,32 @@ logger = logging.getLogger(__name__)
 
 #IMAGE_CAPTURE_URL = 'http://192.168.0.237:8080/photoaf.jpg'
 IMAGE_CAPTURE_URL = 'http://192.168.43.194:8080/photoaf.jpg'
+
+
+class ImageCaptureError(Exception):
+    pass
+
+class AndroidIPCamera:
+    def acquireImage(self, imgname):
+        logger.debug("Calling %s", IMAGE_CAPTURE_URL)
+        res = requests.get(IMAGE_CAPTURE_URL)
+        logger.debug(res)
+        if not res:
+            logger.error("Failed to get image from IP Webcam, no response received")
+            return False
+        if res.status_code != 200:
+            logger.error("Failed to get image from IP Webcam, service returned status %s", res.status_code)
+            return False
+
+        return res.content
+
+
+class FileCacheCamera:
+    def acquireImage(self, imgname):
+        path = 'cache/{0}'.format(imgname)
+        logger.info("Loading image from %s", path)
+        with open(path, 'rb') as f:
+            return f.read()
 
 
 # Define notification event for thread completion
@@ -39,7 +66,7 @@ class CommandEvent(wx.PyEvent):
          """Init Result Event."""
          wx.PyEvent.__init__(self)
          self.SetEventType(EVT_COMMAND_ID)
-         logger.debug(data)
+         #logger.debug(data)
          self.data = data
 
 
@@ -52,6 +79,8 @@ class WindowFrame(wx.Frame):
         self.panel = wx.Panel(self, wx.ID_ANY, pos=(0, 0), size=self.GetSize(), style=wx.NO_BORDER)
         self.panel.SetBackgroundColour("#000")
         self.panel.Bind(wx.EVT_KEY_DOWN, self.parent.onKeyPress)
+        cursor = wx.StockCursor(wx.CURSOR_BLANK)
+        self.SetCursor(cursor)
 
 
     def displayImage(self, image):
@@ -80,6 +109,7 @@ class MainApp(wx.App):
     windows = {}
 
     def OnInit(self):
+        self.cond = Condition()
         screen_width = 1280
         screen_height = 768
 
@@ -120,7 +150,7 @@ class MainApp(wx.App):
             frame.Show()
             logger.info('Creating window "%s" on display %s with size %s x %s offset %s %s', title, i, size[0], size[1], topleft[0], topleft[1])
             self.frames[i] = frame
-        self.worker = WorkerThread(self, self.windows, args)
+        self.worker = WorkerThread(self, self.windows, args, self.cond)
         return True
 
 
@@ -128,17 +158,20 @@ class MainApp(wx.App):
         keycode = event.GetKeyCode()
         print keycode
         if keycode == wx.WXK_SPACE:
-            self.worker.run()
+            with self.cond:
+                self.cond.notify()
         if keycode == wx.WXK_ESCAPE:
             self.Close()
+            self.worker.abort()
         event.Skip()
+
 
     def onCommand(self, event):
         if not event.data:
             logger.error("No data received, exiting")
             self.Close()
         logger.debug("Received command")
-        logger.debug(event.data)
+        #logger.debug(event.data)
         action = event.data['action']
         if action == 'displayImage':
             self.frames[event.data['window']].displayImage(event.data['image'])
@@ -152,9 +185,6 @@ class MainApp(wx.App):
             return
 
         event.Skip()
-        if self.args.autorun:
-            wx.CallLater(100, self.worker.run)
-
 
 
     def Close(self):
@@ -170,6 +200,7 @@ class MainApp(wx.App):
         parser.add_argument('-q', '--quiet', action='store_true', help="Output errors only")
 
         parser.add_argument('-a', '--autorun', action='store_true', help="Do not stop after each step")
+        parser.add_argument('-C', '--usecache', action='store_true', help="Use images from cache")
 
         parser.add_argument('-W', '--width', type=int, help="Target screen width. This will be the width of the output image.", default=1920)
         parser.add_argument('-H', '--height', type=int, help="Target screen height. This will be the height of the output image.", default=1080)
@@ -191,130 +222,166 @@ class MainApp(wx.App):
 
 
 class WorkerThread(Thread):
-    """Worker Thread Class."""
-    state = {'win': 0, 'op': 0}
-
-    def __init__(self, gui, windows, args):
-        """Init Worker Thread Class."""
+    def __init__(self, gui, windows, args, cond):
         Thread.__init__(self)
         self.gui = gui
         self.windows = windows
         self._want_abort = 0
         self.args = args
-
-        #self.start()
+        self.cond = cond
+        if args.usecache:
+            self.camera = FileCacheCamera()
+        else:
+            self.camera = AndroidIPCamera()
+        self.start()
 
 
     def run(self):
-        res = None
-        win = self.state['win']
-        op = self.state['op']
-        logger.debug("Win: %s, op: %s", win, op)
+        rows = 40
+        cols = 24
+        cnrx = 35
+        cnry = 15
+        try:
+            for wname in self.windows.keys():
+                w = self.windows[wname]
 
-        if win >= len(self.windows):
+                if wname not in self.args.displays:
+                    logger.info("Skipping display %s", wname)
+                    self.sendCommand('displayColor', window=wname, color='#000')
+                    continue
+
+                logger.info("Processing display %s", wname)
+                cb = self.drawChessboard(w['width'], w['height'], rows, cols, '#fff', '#000')
+                with open('cb1.png', 'wb') as f: f.write(cb.getvalue())
+                self.sendCommand('displayImage', window=wname, image=cb)
+                self.wait()
+
+                img1 = self.acquireImage("window{0}-img{1}.png".format(wname, 1), w['width'], w['height'])
+                self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img1))
+                self.wait()
+
+                cb = self.drawChessboard(w['width'], w['height'], rows, cols, '#000', '#fff')
+                with open('cb2.png', 'wb') as f: f.write(cb.getvalue())
+                self.sendCommand('displayImage', window=wname, image=cb)
+                self.wait()
+
+                img2 = self.acquireImage("window{0}-img{1}.png".format(wname, 2), w['width'], w['height'])
+                self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img2))
+                self.wait()
+
+
+                img = cv2.subtract(img1, img2)
+                self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
+                cv2.imwrite("window{0}-img{1}.png".format(wname, 3), img)
+                imgdiff1 = img
+                self.wait()
+
+                img = cv2.subtract(img2, img1)
+                self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
+                cv2.imwrite("window{0}-img{1}.png".format(wname, 4), img)
+                imgdiff2 = img
+                self.wait()
+
+                img = cv2.add(imgdiff1, imgdiff2)
+                self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
+                cv2.imwrite("window{0}-img{1}.png".format(wname, 5), img)
+                imgdiff = img
+                self.wait()
+
+                img = cv2.cvtColor(imgdiff, cv2.COLOR_BGR2GRAY)
+
+                img = cv2.medianBlur(img, 13)
+                #blur = cv2.GaussianBlur(img,(5,5),0)
+                ret, mask = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+                self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(mask))
+                cv2.imwrite("window{0}-img{1}.png".format(wname, 6), mask)
+                self.wait()
+
+
+                img = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+                ret, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+                #img = cv2.add(img, mask)
+                self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
+                cv2.imwrite("window{0}-img{1}.png".format(wname, 7), img)
+                self.wait()
+
+                logger.info("Looking for %s x %s inside corners", cnrx, cnry)
+                status, data = cv2.findChessboardCorners(img, (cnrx, cnry), flags=cv2.cv.CV_CALIB_CB_ADAPTIVE_THRESH)
+                if status == False:
+                    logger.error("Failed to parse checkerboard pattern in image")
+                else:
+                    logger.info("Checkerboard found")
+                    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+                    cv2.drawChessboardCorners(vis, (cnrx, cnry), data, status)
+                    self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(vis))
+                    cv2.imwrite("window{0}-img{1}.png".format(wname, '-chess1'), vis)
+
+                    self.wait(1)
+
+                img = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+                ret, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+                #img = cv2.add(img, mask)
+                self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
+                cv2.imwrite("window{0}-img{1}.png".format(wname, 8), img)
+                self.wait()
+
+                status, data = cv2.findChessboardCorners(img, (cnrx, cnry), flags=cv2.cv.CV_CALIB_CB_ADAPTIVE_THRESH)
+                if status == False:
+                    logger.error("Failed to parse checkerboard pattern in image")
+                else:
+                    logger.info("Checkerboard found")
+                    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+                    cv2.drawChessboardCorners(vis, (cnrx, cnry), data, status)
+                    self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(vis))
+                    cv2.imwrite("window{0}-img{1}.png".format(wname, '-chess2'), vis)
+                    self.wait(1)
+
+                self.sendCommand('displayColor', window=wname, color='#000')
+
+
+                #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+                #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
+                #img3 = cv2.fastNlMeansDenoising(img3,None,40,17,31)
+
+
+        except Exception as e:
+            logger.error("Caught exception, exiting")
+            logger.error(traceback.format_exc())
+        finally:
             self.sendCommand('exit')
+
+    def acquireImage(self, imgname, width, height):
+        img = img_string_to_cv(self.camera.acquireImage(imgname))
+        if img is None:
+            raise ImageCaptureError("Failed to read image %s", imgname)
+        img = img_cv_resize(img, width, height, 'outer')
+        cv2.imwrite(imgname, img)
+        return img
+
+
+    def wait(self, sleep=0):
+        time.sleep(sleep)
+        if self.args.autorun:
             return
-
-        wname = self.windows.keys()[win]
-        w = self.windows.values()[win]
-        logger.debug(self.windows.keys())
-
-        if wname not in self.args.displays:
-            logger.info("Skipping display %s", wname)
-            self.sendCommand('displayColor', window=wname, color='#000')
-            self.state['win'] += 1
-            self.state['op'] = 0
-            return
-
-        logger.info("Processing window %s", wname)
-
-        if op == 0:
-            img = self.drawChessboard(w['width'], w['height'], 40, 24, '#fff', '#000')
-            self.sendCommand('displayImage', window=wname, image=img)
-
-        elif op == 1:
-            img = img_string_to_cv(self.acquireImage())
-            if img is None:
-                logger.error("Failed to read camera image")
-                self.sendCommand('exit')
-                return
-            img = img_cv_resize(img, w['width'], w['height'], 'outer')
-            self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
-            cv2.imwrite("window{0}-img{1}.png".format(wname, 1), img)
-            self.img1 = img
-
-        elif op == 2:
-            img = self.drawChessboard(w['width'], w['height'], 40, 24, '#000', '#fff')
-            self.sendCommand('displayImage', window=wname, image=img)
-
-        elif op == 3:
-            img = img_string_to_cv(self.acquireImage())
-            if img is None:
-                logger.error("Failed to read camera image")
-                self.sendCommand('exit')
-                return
-            img = img_cv_resize(img, w['width'], w['height'], 'outer')
-            self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
-            cv2.imwrite("window{0}-img{1}.png".format(wname, 2), img)
-            self.img2 = img
-
-        elif op == 4:
-            img = cv2.subtract(self.img1, self.img2)
-            self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
-            cv2.imwrite("window{0}-img{1}.png".format(wname, 3), img)
-            self.imgdiff1 = img
-
-        elif op == 5:
-            img = cv2.subtract(self.img2, self.img1)
-            self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
-            cv2.imwrite("window{0}-img{1}.png".format(wname, 4), img)
-            self.imgdiff2 = img
-
-        elif op == 6:
-            img = cv2.add(self.img2, self.img1)
-            self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
-            cv2.imwrite("window{0}-img{1}.png".format(wname, 5), img)
-            self.imgdiff = img
+        logger.debug('worker sleep')
+        with self.cond:
+            self.cond.wait()
+        logger.debug('worker wake')
+        if self._want_abort:
+            exit()
 
 
-        elif op == 7:
+    def abort(self):
+        self._want_abort = 1
+        with self.cond:
+            self.cond.notify()
 
-            img = cv2.cvtColor(self.imgdiff, cv2.COLOR_BGR2GRAY)
-            ret, img = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY)
-            img = cv2.medianBlur(img, 3)
-            self.sendCommand('displayImage', window=wname, image=img_cv_to_stringio(img))
-            cv2.imwrite("window{0}-img{1}.png".format(wname, 6), img)
-
-
-        else:
-            self.sendCommand('displayColor', window=wname, color='#000')
-            self.state['win'] += 1
-            self.state['op'] = 0
-            return
-
-        self.state['op'] += 1
 
     def sendCommand(self, action, **kwargs):
         logger.debug("Sending command %s", action)
-        logger.debug(kwargs)
-        logger.debug("end of kwargs")
         res = kwargs
         res['action'] = action
         wx.PostEvent(self.gui, CommandEvent(res))
-
-
-    def acquireImage(self):
-        logger.debug("Calling %s", IMAGE_CAPTURE_URL)
-        res = requests.get(IMAGE_CAPTURE_URL)
-        logger.debug(res)
-        if not res:
-            logger.error("Failed to get image, no response received")
-            return False
-        if res.status_code != 200:
-            logger.error("Failed to get image, service returned status %s", res.status_code)
-            return False
-
-        return res.content
 
 
     def drawChessboard(self, screen_width, screen_height, cols, rows, color1='#fff', color2='#000'):
@@ -324,6 +391,7 @@ class WorkerThread(Thread):
         image = wand.image.Image(width=screen_width, height=screen_height, background=wand.color.Color(color1))
         with wand.drawing.Drawing() as draw:
             draw.fill_color = wand.color.Color(color2)
+            draw.color = wand.color.Color(color2)
             for r in range(rows):
                 for c in range(cols):
                     if not (c + r) % 2:
@@ -332,7 +400,7 @@ class WorkerThread(Thread):
                     y = square_height * r
 
                     # logger.debug("%s %s %s %s", x, y, square_width, square_height)
-                    draw.rectangle(x, y, width=square_width, height=square_height)
+                    draw.rectangle(x, y, width=square_width-1, height=square_height-1)
 
             draw.draw(image)
             blob = image.make_blob('png')
@@ -341,44 +409,6 @@ class WorkerThread(Thread):
 
 
 
-
-def main():
-    img1 =  cv2.imread(args.infile1, cv2.CV_8UC1)
-    img2 =  cv2.imread(args.infile2, cv2.CV_8UC1)
-    #img3 = img1 - img2;
-    imgdiff = cv2.subtract(img2, img1)
-
-    cv2.imshow("test",imgdiff)
-    key=cv2.waitKey(0)
-
-
-    img1 = cv2.resize(img1,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
-    img2 = cv2.resize(img2,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
-
-    img1 = cv2.medianBlur(img1, 5)
-    img2 = cv2.medianBlur(img1, 5)
-
-    cv2.imshow("test",img1)
-    key=cv2.waitKey(0)
-
-    ret, img2 = cv2.threshold(img1, 30, 255, cv2.THRESH_BINARY)
-
-    cv2.imshow("test",img2)
-    key=cv2.waitKey(0)
-
-    #img3 = cv2.adaptiveThreshold(img1, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-    img1 = cv2.adaptiveThreshold(img1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,31,3)
-
-    cv2.imshow("test",img1)
-    key=cv2.waitKey(0)
-
-    img3 = cv2.subtract(img2, img1)
-    #img3 = cv2.fastNlMeansDenoising(img3,None,40,17,31)
-    cv2.imwrite(args.outfile, img3)
-    cv2.imshow("test",img3)
-    key=cv2.waitKey(0)
-
-    sys.exit(0)
 
 def img_string_to_cv(string):
     nparr = np.fromstring(string, np.uint8)
